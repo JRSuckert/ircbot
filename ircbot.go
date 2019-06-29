3d229cfa680ba68520ffbe8f2b37f235b8f1ffe3
Jens Rene Suckert <jens.suckert@gmail.com>
2019-06-29 17:36:09 +0200

    Added parsing options on messages. Added option to join channels listed in config.yml

--- ircbot.go
+++ ircbot.go
@@ -5,10 +5,12 @@ import (
 	"bytes"
 	"fmt"
 	"net"
+	"strings"
 )
 
 type IRCConnection struct {
-	connection net.Conn
+	connection  net.Conn
+	established bool
 }
 
 func (irc *IRCConnection) Connect(config Config) {
@@ -19,16 +21,20 @@ func (irc *IRCConnection) Connect(config Config) {
 	}
 	fmt.Fprintf(irc.connection, "NICK %s\r\n", config.Nick)
 	fmt.Fprintf(irc.connection, "USER %s 0 * %s\r\n", config.Nick, config.Nick)
+	fmt.Fprintf(irc.connection, "PRIVMSG NickServ IDENTIFY %s\r\n", config.Pass)
+
 }
 
-func (irc *IRCConnection) Receive(ch chan bool) {
+func (irc *IRCConnection) Receive(EOC chan bool, messages chan string) {
+	defer close(EOC)
+	defer close(messages)
 	scanner := bufio.NewScanner(bufio.NewReader(irc.connection))
 	split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
 		eolRune := []rune{'\r', '\n'}
 		eol := []byte(string(eolRune))
 		for i := 0; i < len(data)-1; i++ {
 			if bytes.Equal(data[i:i+2], eol) {
-				return i + 1, data[:i+1], nil
+				return i + 2, data[:i+1], nil
 			}
 		}
 		return 0, nil, nil
@@ -36,9 +42,31 @@ func (irc *IRCConnection) Receive(ch chan bool) {
 	scanner.Split(split)
 	for scanner.Scan() {
 		text := scanner.Text()
-		fmt.Printf("%s\n", text)
+		messages <- text
 	}
-	ch <- false
+	EOC <- true
+}
+
+func (irc *IRCConnection) Parse(messages chan string) {
+	for c := range messages {
+		words := strings.Fields(c)
+		var hostname string
+		if c[0] == ':' {
+			hostname = words[0]
+			words = words[1:]
+		}
+		if strings.EqualFold(words[0], "004") {
+			irc.established = true
+		}
+		if strings.EqualFold(words[0], "PING") {
+			fmt.Println(c)
+			responseText := strings.Replace(c, "PING", "PONG", 1)
+			irc.connection.Write([]byte(responseText))
+		} else {
+			fmt.Printf("[%s] %s \n", hostname, strings.Join(words, " "))
+		}
+
+	}
 }
 
 func main() {
@@ -48,13 +76,26 @@ func main() {
 	var server IRCConnection
 	server.Connect(config)
 
-	ch := make(chan bool)
+	eoc := make(chan bool)
+	messages := make(chan string, 512)
 
-	go server.Receive(ch)
+	go server.Receive(eoc, messages)
+	go server.Parse(messages)
 
 	for {
-		if !<-ch {
+		if server.established {
 			break
+		}
+	}
+
+	for _, channel := range config.Channels {
+		fmt.Printf("Trying to join %s\n", channel)
+		fmt.Fprintf(server.connection, "JOIN %s\r\n", channel)
+	}
+
+	for e := range eoc {
+		if e {
+			fmt.Println("Received exit")
 		}
 	}
 }
